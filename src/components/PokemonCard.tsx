b38c09bb968699c13deff7875d20f654e7015373
heyberger.augustin@gmail.com <heyberger.augustin@gmail.com>
2024-10-24 09:47:33 +0200

    étape2

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PokemonCard: React.FC = () => {
+  return (
+    <figure>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon Image" />
+      <figcaption>bulbasaur</figcaption>
+    </figure>
+  );
+};
+
+export default PokemonCard;
