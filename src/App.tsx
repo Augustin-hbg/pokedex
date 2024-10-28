0e184fcffabb7af9aadf10e1623388b96edad24d
heyberger.augustin@gmail.com <heyberger.augustin@gmail.com>
2024-10-28 10:35:21 +0100

    state.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 
 const pokemonList = [
@@ -7,16 +8,55 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
-    imgSrc: "", 
+    imgSrc: "",
   },
 ];
 
 function App() {
+  
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  // Fonction pour afficher le Pokémon suivant
+  const handleNextPokemon = () => {
+    setPokemonIndex((prevIndex) => (prevIndex + 1) % pokemonList.length);
+  };
+
+  // Fonction pour afficher le Pokémon précédent
+  const handlePreviousPokemon = () => {
+    setPokemonIndex((prevIndex) =>
+      prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <>
-         <PokemonCard pokemon={pokemonList[0]} /> {/* Premier Pokémon */}
-         <PokemonCard pokemon={pokemonList[1]} /> {/* Deuxième Pokémon */}
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
+      {/* Bouton Précédent affiché uniquement si pokemonIndex > 0 */}
+      {pokemonIndex > 0 && (
+        <button onClick={handlePreviousPokemon}>Précédent</button>
+      )}
+
+      {/* Bouton Suivant affiché uniquement si pokemonIndex < pokemonList.length - 1 */}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={handleNextPokemon}>Suivant</button>
+      )}
     </>
   );
 }
